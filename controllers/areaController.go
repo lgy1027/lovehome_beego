22237328afcbcff4427e06f450d6c797238f5bf0
liguoyu <lgy10271416@gmail.com>
2018-12-11 17:32:20 +0800

    错误处理，url、area修复

--- controllers/areaController.go
+++ controllers/areaController.go
@@ -1,8 +1,13 @@
 package controllers
 
 import (
+	"encoding/json"
 	"github.com/astaxie/beego"
-	"net/http"
+	"github.com/astaxie/beego/cache"
+	_ "github.com/astaxie/beego/cache/redis"
+	"github.com/astaxie/beego/orm"
+	"lovehome/models"
+	"time"
 )
 
 // 区域管理
@@ -10,7 +15,57 @@ type AreaController struct {
 	beego.Controller
 }
 
-func (c *AreaController) Get(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
+// 转换成json数据返回
+func (c *AreaController) RetData(resp map[string]interface{}) {
+	c.Data["json"] = &resp
+	c.ServeJSON()
+}
 
+func (c *AreaController) GetArea() {
+	beego.Info("connect success")
+
+	resp := make(map[string]interface{})
+	resp["errno"] = models.RECODE_OK
+	resp["errmsg"] = models.RecodeText(models.RECODE_OK)
+	defer c.RetData(resp)
+
+	//从redis缓存中拿数据拿数据
+	cache_conn, err := cache.NewCache("redis", `{"key":"lovehome","conn":":6379","dbNum":"0"}`)
+
+	//if areaData := cache_conn.Get("area");areaData!=nil{
+	//	beego.Info("get data from cache===========")
+	//	resp["data"] = areaData
+	//	return
+	//}
+
+	//从mysql数据库拿到area数据
+	var areas []models.Area
+
+	o := orm.NewOrm()
+	num, err := o.QueryTable("area").All(&areas)
+
+	if err != nil {
+		resp["errno"] = models.RECODE_DBERR
+		resp["errmsg"] = models.RecodeText(models.RECODE_DBERR)
+		return
+	}
+	if num == 0 {
+		resp["errno"] = 4002
+		resp["errmsg"] = "没有查到数据"
+		return
+	}
+
+	resp["data"] = areas
+
+	//把数据转换成json格式存入缓存
+	json_str, err := json.Marshal(areas)
+	if err != nil {
+		beego.Info("encoding err")
+		return
+	}
+
+	cache_conn.Put("area", json_str, time.Second*3600)
+
+	//打包成json返回给前段
+	beego.Info("query data sucess ,resp =", resp, "num =", num)
 }
--- routers/router.go
+++ routers/router.go
@@ -6,7 +6,20 @@ import (
 )
 
 func init() {
+	//首页
 	beego.Router("/", &controllers.MainController{})
-
+	// 区域获取
 	beego.Router("/api/v1.0/areas", &controllers.AreaController{}, "get:GetArea")
+
+	beego.Router("/api/v1.0/houses/index", &controllers.HouseIndexController{}, "get:GetHouseIndex")
+
+	//api/v1.0/session
+	beego.Router("/api/v1.0/session", &controllers.SessionController{}, "get:GetSessionData;delete:DeleteSessionData")
+
+	//api/v1.0/users
+	beego.Router("/api/v1.0/users", &controllers.UserController{}, "post:Reg")
+
+	//api/v1.0/sessions
+	beego.Router("/api/v1.0/sessions", &controllers.SessionController{}, "post:Login")
+
 }
